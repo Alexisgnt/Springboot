04be252d3c72c8f05bf0468953ca302c77e87818
Alexis GENET <genet.alexis49@gmail.com>
2025-03-12 09:52:50 +0100

    Initial commit

---
+++ rbb/rbb/src/main/java/com/rbb/rbb/RbbApplication.java
@@ -0,0 +1,13 @@
+package com.rbb.rbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbbApplication.class, args);
+	}
+
+}
---
+++ rbb/rbb/src/test/java/com/rbb/rbb/RbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbb.rbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
